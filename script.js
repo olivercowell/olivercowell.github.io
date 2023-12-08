6e2ee8b10877fdfc2540992157bedd43a87afc6b
Oliver Cowell <oliver.cowell44@gmail.com>
2023-12-08 18:24:02 +1000

    Start adding dice simulator

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function throwDice() {
   let result = Math.floor(Math.random() * 7);
   return result;
 }
+
+function getRandomNumber(min, max, divisibleBy) {
+  do {
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
+  } while (result % divisibleBy != 0);
+  return result;
+}
+
+function displayRandomNumber() {
+  let number = getRandomNumber(6, 10, 2);
+  let element = document.getElementById("result");
+  element.innerHTML = String(number);
+}
