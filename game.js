5b5d3558dd9bfa08d98b36b423b4e9b7e6d91273
Oliver Cowell <oliver.cowell44@gmail.com>
2023-12-10 13:49:57 +1000

    Create dice simulator

--- game.js
+++ game.js
@@ -14,6 +14,20 @@ function displayRandomNumber() {
   let min = parseInt(document.getElementById("min").value);
   let max = parseInt(document.getElementById("max").value);
   let divBy = parseInt(document.getElementById("divBy").value);
+  if (max <= min) {
+    alert("WARNING: Max must be more than Min");
+    return;
+  }
+  let containsDivisible = false;
+  for (let i = min; i <= max; ++i) {
+    if (i % divBy == 0) {
+      containsDivisible = true;
+    }
+  }
+  if (!containsDivisible) {
+    alert("WARNING: No numbers in range are divisible");
+    return;
+  }
   let number = getRandomNumber(min, max, divBy);
   let element = document.getElementById("result");
   element.innerHTML = String(number);
